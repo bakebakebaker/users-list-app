3a167a72c0219f213f9a6686862a1e9e89f7e7c9
Przemysław Kulczak <przekulczak@gmail.com>
2021-02-17 22:32:05 +0100

    add spinner

--- src/common-components/Spinner/index.tsx
+++ src/common-components/Spinner/index.tsx
@@ -1,3 +1,60 @@
-const Spinner = () => <div className="spinner" title="spinner" />;
+import React from "react";
+import styled from "styled-components";
+
+const Spinner = () => (
+  <SpinnerWrapper title="spinner">
+    <StyledSpinner viewBox="0 0 50 50">
+      <circle
+        className="path"
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        strokeWidth="4"
+      />
+    </StyledSpinner>
+  </SpinnerWrapper>
+);
+
+const SpinnerWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+`;
+
+const StyledSpinner = styled.svg`
+  animation: rotate 2s linear infinite;
+  margin: -25px 0 0 -25px;
+  width: 200px;
+  height: 200px;
+
+  & .path {
+    stroke: #000;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`;
 
 export default Spinner;
