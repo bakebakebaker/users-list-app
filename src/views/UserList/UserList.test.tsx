98a8167691cf62cd580bd75122ca16bb7c2a0616
Przemysław Kulczak <przekulczak@gmail.com>
2021-02-17 22:41:59 +0100

    test spinner

--- src/views/UserList/UserList.test.tsx
+++ src/views/UserList/UserList.test.tsx
@@ -15,74 +15,10 @@ describe("User list app", () => {
     const header = queryByTitle("main-header");
     expect(header?.innerHTML).toBe("Users list");
   });
-  /*
+
   it("render spinner while fetching data", () => {
     const { queryByTitle } = render(<Spinner />);
-    expect(queryByTitle).toBeInTheDocument();
+    const spinner = queryByTitle("spinner");
+    expect(spinner).toBeInTheDocument();
   });
-
-  it("render list", () => {
-    fetchMock.mockResponse(
-      JSON.stringify([
-        {
-          id: 1,
-          name: "Leanne Graham",
-          username: "Bret",
-          email: "Sincere@april.biz",
-          address: {},
-          phone: "1-770-736-8031 x56442",
-          website: "hildegard.org",
-          company: {},
-        },
-        {
-          id: 2,
-          name: "Ervin  Howell",
-          username: "Antonette",
-          email: "Shanna@melissa.tv",
-          address: {},
-          phone: "010-692-6593 x09125",
-          website: "anastasia.net",
-          company: {},
-        },
-      ])
-    );
-    const { findByTestId } = render(<UsersList />);
-    expect(findByTestId("user-item-1")).toBeInTheDocument();
-  });
-
-  it("should render only proper user after filtering", async () => {
-    fetchMock.mockResponse(
-      JSON.stringify([
-        {
-          id: 1,
-          name: "Leanne Graham",
-          username: "Bret",
-          email: "Sincere@april.biz",
-          address: {},
-          phone: "1-770-736-8031 x56442",
-          website: "hildegard.org",
-          company: {},
-        },
-        {
-          id: 2,
-          name: "Ervin  Howell",
-          username: "Antonette",
-          email: "Shanna@melissa.tv",
-          address: {},
-          phone: "010-692-6593 x09125",
-          website: "anastasia.net",
-          company: {},
-        },
-      ])
-    );
-    const { findByTestId, findAllByTestId } = render(<UsersListApp />);
-
-    const input = await findByTestId("search-input");
-
-    fireEvent.change(input, { target: { value: "Leanne" } });
-
-    const items = await findAllByTestId(/user-item/i);
-    expect(items.length).toBe(1);
-  });
-*/
 });
